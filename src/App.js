936a4ea6ee97225116b41c3bb4be06693389eda0
Praveen Byreddy <preddy@incapsulate.com>
2024-01-05 01:35:42 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Button } from '@salesforce/design-system-react'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button label="Hello Button" />
+    </div>
+  );
+}
+
+export default App;
